2d4b865d46173c48e31a52050c49ab017adac5e3
tanakaderoy <tanaka.deroy@gmail.com>
2020-08-08 04:35:20 -0400

    Fixing deployment issue

--- controllers/watch-controller.js
+++ controllers/watch-controller.js
@@ -15,7 +15,7 @@ const getVideoUrl = async (url) => {
     const browser = await puppeteer.launch({  args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
-    ],});
+    ],headless: true});
     const page = await browser.newPage();
     await page.goto("https://" + url.replace('"', ""));
 
